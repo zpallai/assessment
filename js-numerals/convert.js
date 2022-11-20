c780a37a636ad40d6505577b2cda7ff71a2bc09b
Zoltan Pallai <zoli.pallai@Konyvecske.local>
2022-11-20 19:45:22 +0100

    Conversion functions v2, test module, testing, additional styles.

--- js-numerals/convert.js
+++ js-numerals/convert.js
@@ -4,18 +4,23 @@
 const first = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
 const second = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-const higher = [" ", " thousand ", " million ", " billion ", "trillion ", " googleplex "];
+const higher = [" ", " thousand ", " million ", " billion ", " trillion ", " would think it's a googolplex "];
 
 // Return all digits less than a thousand
-function to_thousand(number) {
+export function to_thousand(number) {
     let og_number = number;
+    let str_number = number.toString();
     while( number.charAt(0) === '0')
     {
       number = number.substring(1);
     }
     number = parseInt(number);
+
+    if (og_number == "00" || og_number === "000") {
+        return "";
+    }
     
-    if (number <= 10) {
+    else if (number <= 10) {
         if (og_number.charAt(1) === '0') {
             return "and " + first[number];
         }
@@ -25,12 +30,21 @@ function to_thousand(number) {
     }
 
     else if (number > 10 && number < 20) {
-        return teens[og_number[-1]];
+        number = number.toString();
+        if (str_number[0] == "0") {
+            return "and " + teens[number[1]];
+        }
+        else {
+            return teens[number[1]];
+        }
     }
 
     else if (number >= 20 && number < 100) {
         number = number.toString();
-        if (number[1] == 0) {
+        if (str_number[2] == 0) {
+            return "and " + second[number[0]];
+        }
+        else if (number[1] == 0) {
             return second[number[0]];
         }
         else {
@@ -59,7 +73,7 @@ function to_thousand(number) {
 }
 
 // Split numbers on thousands place (comma separator)
-function split_thousands(number) {
+export function split_thousands(number) {
     let temp = [];
     let final = [];
     let x = 0;
@@ -80,14 +94,20 @@ function split_thousands(number) {
 }
 
 // Assemble final string by converting numbers split by thousands
-function final_words(number) {
+export function final_words(number) {
     let word = "";
     let words = "";
     let number_split = split_thousands(number);
     let thousands = number_split.length;
+    if (number === 0) {
+        return "zero";
+    }
     for (var x = 0; x < number_split.length; x++ ) {
         word = to_thousand(number_split[x]);
-        if (thousands > 1) {
+        if (number_split[x] == "0" || number_split[x] == "00" || number_split[x] === "000") {
+            continue;
+        }
+        else if (thousands > 1) {
             words += word + higher[thousands - 1];
             thousands--;
         }
@@ -97,9 +117,6 @@ function final_words(number) {
     }
     return words;
 }
-
-console.log(final_words(1300420));
-
 
 
 
---
+++ js-numerals/test.js
@@ -0,0 +1,44 @@
+// Algorithm Tests
+import { final_words } from "/convert.js";
+
+function test(parameter, expected) {
+    let output = final_words(parameter);
+    try {
+    final_words(parameter);
+    } catch (e) {
+    console.log(e);
+    } finally {
+    console.log("Input: " + parameter +  "\n");
+    console.log("Expected: " + expected +  "\n");
+    console.log("Output: " + output +  "\n\n");
+    }
+}
+
+test(42, "forty-two")
+test(1999, "one thousand nine hundred and ninety-nine")
+test(2001, "two thousand and one")
+test(17999, "seventeen thousand nine hundred and ninety-nine")
+test(100001, "one hundred thousand and one")
+test(342251, "three hundred and forty-two thousand two hundred and fifty-one")
+test(1300420, "one million three hundred thousand four hundred and twenty")
+
+test(14, "fourteen")
+test(20, "twenty")
+test(1014, "one thousand and fourteen")
+test(100, "one hundred")
+test(303030, "three hundred and three thousand and thirty")
+test(711111111, "seven hundred and eleven million one hundred and eleven thousand one hundred and eleven")
+test(1234567890123, "one trillion two hundred and thirty-four billion five hundred and sixty-seven million eight hundred and ninety thousand one hundred and twenty-three")
+test(1000, "one thousand")
+test(100000000, "one hundred million")
+test(1000000000000000, "one would think it's a googolplex")
+
+
+
+
+
+
+
+
+
+
